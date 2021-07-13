3fbc3bf959c780517f32707efb9516e6fdf0b389
zahreddinenouri <zahreddine.nouri16]gmail.com>
2021-07-13 09:51:47 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function scoreToGrade(x) { 
+if  (0< x <20) { return 'A';}
+     else if (20 < x < 40) { return 'B'; }
+      else if (40 < x < 60) { return 'C'; } 
+    else if (60 < x <80) { return 'D'; } 
+    else if (80 <x <=100) { return 'E' ; }
+    else if (x<0) {return  'invaild'; }
+    else if (x> 100) { return 'invaild' ; } }
